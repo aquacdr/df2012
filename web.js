94adba71c3171013c9c46f9eb1f94f3fbb7153ff
Matt Fisher <mfisher@appirio.com>
2012-08-24 17:15:52 -0500

    init

---
+++ web.js
@@ -0,0 +1,15 @@
+var express = require('express');
+
+var app = express.createServer(express.logger());
+
+app.get('/', function(request, response) {
+  response.send('Hello World!');
+});
+
+var port = process.env.PORT || 30000;
+
+app.listen(port, function(){
+  console.log("Express server listening on port %d", app.address().port);
+
+});
+
